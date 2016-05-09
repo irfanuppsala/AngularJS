ba553c7af7188b9b9af3e1000c903c3ee649eff9
Irfan <irfan.uppsala@gmail.com>
2016-05-09 13:57:45 +0200

    Added AngularJS example files

---
+++ js/controllers/MainController.js
@@ -0,0 +1,8 @@
+app.controller('MainController', ['$scope', function($scope) { 
+	$scope.num1 = 0; 
+	$scope.num2 = 0;
+	$scope.num3 = 0;
+	$scope.num4 = 0;
+	$scope.num5 = 0;
+	$scope.num6 = 0; 
+});
